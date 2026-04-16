e8befcb90e47de1ed315a9b1e1b909c8a8f6ce69
fengqiao <yiqiaofeng@xgd.com>
2026-04-16 10:30:07 +0800

    feature:更新版本号为 1.4.0

--- woocommerce-paykka-gateway.php
+++ woocommerce-paykka-gateway.php
@@ -4,7 +4,7 @@
  * Plugin Name:       PayKKa for WooCommerce
  * Plugin URI:        https://github.com/1404369980/paykka-for-woocommerce
  * Description:       PayKKa Hosted 收银台支付（跳转 Paykka 完成付款），支持 WooCommerce 结账与 Blocks。
- * Version:           1.3.1
+ * Version:           1.4.0
  * Author:            Fengqiao Yi
  * Author URI:        https://github.com/1404369980/paykka-for-woocommerce
  * License:           GNU General Public License v3.0
